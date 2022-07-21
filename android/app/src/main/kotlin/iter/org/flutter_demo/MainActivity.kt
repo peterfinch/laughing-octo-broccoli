ab2613c000c55038c952af1fd0501f3511e4039a
Peter Finch <peterfinch@hotmail.com>
2022-07-21 16:44:21 +0200

    First

---
+++ android/app/src/main/kotlin/iter/org/flutter_demo/MainActivity.kt
@@ -0,0 +1,6 @@
+package iter.org.flutter_demo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/widgets/NavDrawer.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+class NavDrawer extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: <Widget>[
+          const DrawerHeader(
+            child: Text(
+              'Your apps etc',
+              style: TextStyle(color: Colors.white, fontSize: 20),
+            ),
+            decoration: BoxDecoration(
+                color: Colors.amberAccent,
+                image: DecorationImage(
+                    fit: BoxFit.contain,
+                    image: AssetImage('images/IterLogo.png'),scale: 30)),
+          ),
+          ListTile(
+            leading: const Icon(Icons.input),
+            title: const Text('Welcome'),
+            onTap: () => {},
+          ),
+          ListTile(
+            leading: const Icon(Icons.verified_user),
+            title: const Text('Profile'),
+            onTap: () => {Navigator.of(context).pop()},
+          ),
+          ListTile(
+            leading: const Icon(Icons.settings),
+            title: const Text('Settings'),
+            onTap: () => {Navigator.of(context).pop()},
+          ),
+          ListTile(
+            leading: const Icon(Icons.border_color),
+            title: const Text('About'),
+            onTap: () => {showAboutDialog(context: context,
+            applicationVersion: '3.0.1',
+            applicationLegalese: 'Some legalese....')},
+          ),
+          ListTile(
+            leading: const Icon(Icons.exit_to_app),
+            title: const Text('Logout'),
+            onTap: () => {Navigator.of(context).pop()},
+          ),
+        ],
+      ),
+    );
+  }
+}
